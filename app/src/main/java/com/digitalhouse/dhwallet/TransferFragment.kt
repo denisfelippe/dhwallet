a686c4b51d6f03eadbb41d5f72fd307566143df5
Denis Rocha <denis@digitalhouse.com>
2022-01-20 11:18:59 -0500

    feat: Add camera permission

--- app/src/main/java/com/digitalhouse/dhwallet/TransferFragment.kt
+++ app/src/main/java/com/digitalhouse/dhwallet/TransferFragment.kt
@@ -1,7 +1,13 @@
 package com.digitalhouse.dhwallet
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.digitalhouse.dhwallet.adapter.ContactAdapter
@@ -10,6 +16,16 @@ import com.digitalhouse.dhwallet.model.ContactType
 import com.digitalhouse.dhwallet.util.SharedPref
 
 class TransferFragment : Fragment(R.layout.fragment_transfer) {
+
+    private val permissionsResultCallback = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) {
+        if (it) {
+            Toast.makeText(requireContext(), "Permitido!", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(requireContext(), "Não Permitido!", Toast.LENGTH_LONG).show()
+        }
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -25,5 +41,15 @@ class TransferFragment : Fragment(R.layout.fragment_transfer) {
 
         val recycler = view.findViewById<RecyclerView>(R.id.recycler)
         recycler.adapter = ContactAdapter(listContact, SharedPref(view.context))
+
+        view.findViewById<Button>(R.id.btn_scan).setOnClickListener {
+            val permission = ContextCompat.checkSelfPermission(it.context, Manifest.permission.CAMERA)
+
+            if (permission != PackageManager.PERMISSION_GRANTED) {
+                permissionsResultCallback.launch(Manifest.permission.CAMERA)
+            } else {
+                Toast.makeText(requireContext(), "Já está permitido!", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
